e81489aeede3b310b872df73616fa3a163bd8053
Fraser Molyneux <websites@mx-mail.io>
2022-06-21 22:12:15 +0100

    Fixing script path

--- .azure-pipelines/scripts/CreateAppRegistrations.ps1
+++ .azure-pipelines/scripts/CreateAppRegistrations.ps1
@@ -2,5 +2,5 @@ param (
     $environment
 )
 
-. "/.azure-pipelines/scripts/functions/CreateAppRegistration.ps1" `
+. "./.azure-pipelines/scripts/functions/CreateAppRegistration.ps1" `
     -applicationName "geolocation-lookup-api-$environment"
