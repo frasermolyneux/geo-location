ed6aa2e3a7422d6133f8d816dc7123125466b1bb
Fraser Molyneux <websites@mx-mail.io>
2022-06-22 17:32:14 +0100

    Adding deploy principal permissions

--- .azure-pipelines/scripts/SetPublicWebAppPermissions.ps1
+++ .azure-pipelines/scripts/SetPublicWebAppPermissions.ps1
@@ -6,4 +6,4 @@ param (
 $webApp = (az webapp show --name "webapp-geolocation-public-$environment-$location" --resource-group "rg-geolocation-$environment-$location") | ConvertFrom-Json
 $principalId = $webApp.identity.principalId
 
-. "./.azure-pipelines/scripts/functions/GrantLookupApiPermissionsToApp.ps1" -principalId $principalId -environment $environment
+. "./scripts/functions/GrantLookupApiPermissionsToApp.ps1" -principalId $principalId -environment $environment
