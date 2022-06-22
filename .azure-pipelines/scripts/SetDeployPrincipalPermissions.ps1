458f30032c41678730ba1ced6831c325e2aab84e
Fraser Molyneux <websites@mx-mail.io>
2022-06-22 17:49:26 +0100

    Updating to az account command

--- .azure-pipelines/scripts/SetDeployPrincipalPermissions.ps1
+++ .azure-pipelines/scripts/SetDeployPrincipalPermissions.ps1
@@ -3,6 +3,6 @@ param (
     $location
 )
 
-$principalId = az ad signed-in-user show --query id -o tsv
+$principalId = az account show --query id -o tsv
 
 az keyvault set-policy --name "kv-geoloc-$environment-$location" --spn $principalId --secret-permissions get update
