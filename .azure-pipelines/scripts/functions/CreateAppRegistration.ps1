772eef732f11465e8d91425ae11160e6d99d75ce
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-06 21:03:01 +0100

    Set application to be AzureAdMyOrg only

--- .azure-pipelines/scripts/functions/CreateAppRegistration.ps1
+++ .azure-pipelines/scripts/functions/CreateAppRegistration.ps1
@@ -6,6 +6,8 @@ param (
 az ad app create --display-name $applicationName --identifier-uris "api://$applicationName" | Out-Null
 $applicationId = (az ad app list --filter "displayName eq '$applicationName'" --query '[].appId') | ConvertFrom-Json
 
+az ad app update --id $applicationId --sign-in-audience 'AzureADMyOrg' | Out-Null
+
 $applicationServicePrincipal = az ad sp show --id $applicationId
 if ($null -eq $applicationServicePrincipal) {
     az ad sp create --id $applicationId | Out-Null
