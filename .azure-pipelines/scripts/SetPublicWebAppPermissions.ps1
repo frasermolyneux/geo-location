6ea6b6c4e4eb81adc0ebe32a84492ceb8d3cd647
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-10 07:06:48 +0100

    Update resource group name

--- .azure-pipelines/scripts/SetPublicWebAppPermissions.ps1
+++ .azure-pipelines/scripts/SetPublicWebAppPermissions.ps1
@@ -3,7 +3,7 @@ param (
     $location
 )
 
-$webApp = (az webapp show --name "webapp-geolocation-public-$environment-$location" --resource-group "rg-geolocation-$environment-$location") | ConvertFrom-Json
+$webApp = (az webapp show --name "webapp-geolocation-public-$environment-$location" --resource-group "rg-platform-webapps-prd-uksouth") | ConvertFrom-Json
 $principalId = $webApp.identity.principalId
 
 . "./.azure-pipelines/scripts/functions/GrantLookupApiPermissionsToApp.ps1" -principalId $principalId -environment $environment
