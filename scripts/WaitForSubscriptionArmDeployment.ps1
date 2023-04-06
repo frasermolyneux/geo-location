d7636bd0064a2b6eafebc395012aa19d575c369b
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-04-06 21:40:39 +0100

    Refactor / rework

--- scripts/WaitForSubscriptionArmDeployment.ps1
+++ scripts/WaitForSubscriptionArmDeployment.ps1
@@ -3,12 +3,12 @@ param(
 )
 
 # Check if there is an existing deployment running
-$deploymentOutput = (az deployment group show --name $deploymentName) | ConvertFrom-Json
+$deploymentOutput = (az deployment sub show --name $deploymentName) | ConvertFrom-Json
 $deploymentStatus = $deploymentOutput.properties.provisioningState
 
 while ($deploymentStatus -eq "Running") {
     Write-Host "An existing deployment is running..."
     Start-Sleep -s 10
-    $deploymentOutput = (az deployment group show --name $deploymentName) | ConvertFrom-Json
+    $deploymentOutput = (az deployment sub show --name $deploymentName) | ConvertFrom-Json
     $deploymentStatus = $deploymentOutput.properties.provisioningState
 }
